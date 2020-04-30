d8e0bf7d37812f680fc762efa07160b122b41adc
Andre Carter <carstar2486@gmail.com>
2020-04-30 03:33:40 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,22 @@
-def find_min_value(array)
-  # Add your solution here
+def find_min_value(arr)
+max = 0
+i = 0
+while i < arr.length do
+if arr[i] > max
+max = arr[i]
+end
+i += 1
+end
+
+j = 0
+min = max
+
+while j < arr.length do
+if arr[j] < min
+min = arr[j]
+end
+j += 1
+end
+return min
+
 end
